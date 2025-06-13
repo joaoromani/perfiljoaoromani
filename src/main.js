ea57ccbeb8677f596882cd1e36bec7ab5d667784
João Romani <romani.joao1@gmail.com>
2025-06-13 13:50:27 -0300

    Versão inicial do projeto a partir do VS Code

---
+++ src/main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const images = ["../images/eu.jpg", "../images/eu4.jpg", "../images/eu5.jpg"];
+    let i= 0;
+
+    const mainPhoto = document.getElementById("main-photo");
+    const prevBtn = document.getElementById("botaovoltar");
+    const nextBtn = document.getElementById("botaoseguir");
+
+    prevBtn.addEventListener("click", () => {
+        i = (i - 1 + images.length) % images.length;
+        mainPhoto.src = images[i];
+    });
+
+    nextBtn.addEventListener("click", () => {
+        i = (i + 1) % images.length;
+        mainPhoto.src = images[i];
+    });
+});
